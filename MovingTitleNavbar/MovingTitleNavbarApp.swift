1b899206cdedd50ee3ea210f3dabde723a925e4a
Moamen Hassaballah <hassaballahmoamen84@gmail.com>
2024-08-06 22:50:16 +0300

    Initial Commit

---
+++ MovingTitleNavbar/MovingTitleNavbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingTitleNavbarApp.swift
+//  MovingTitleNavbar
+//
+//  Created by Moamen Hassaballah on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovingTitleNavbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
